9272c0d57ecd09dc10bc0eac13781f7913f0e0bf
Virgilius Santos <virgiliussantos@MacBook-Pro-de-Virgilius.local>
2017-03-23 15:41:24 -0300

    criação da classe tweet

---
+++ challenge_4/Tweet.h
@@ -0,0 +1,27 @@
+//
+//  Tweet.h
+//  challenge_4
+//
+//  Created by Virgilius Santos on 23/03/17.
+//  Copyright © 2017 TP. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+@class User;
+
+
+@interface Tweet : NSObject
+
+
+@property (readonly) int code;
+@property (weak, readonly) User *user;
+@property (copy, readonly) NSDate *date;
+@property (copy, readonly) NSString *tweet;
+@property (readonly) int likesCount;
+
+- (instancetype)initWithCode:(int)code andUser:(User *)user andDate:(NSDate *)date
+                    andTweet:(NSString *)tweet;
+
+-(void)incrementLikesCount;
+
+@end
---
+++ challenge_4/Tweet.m
@@ -0,0 +1,31 @@
+//
+//  Tweet.m
+//  challenge_4
+//
+//  Created by Virgilius Santos on 23/03/17.
+//  Copyright © 2017 TP. All rights reserved.
+//
+
+#import "Tweet.h"
+
+@implementation Tweet
+
+- (instancetype)initWithCode:(int)code andUser:(User *)user andDate:(NSDate *)date
+                    andTweet:(NSString *)tweet
+{
+    self = [super init];
+    if (self) {
+        _code = code;
+        _user = user;
+        _date = [date copy];
+        _tweet = [tweet copy];
+        _likesCount = 0;
+    }
+    return self;
+}
+
+-(void)incrementLikesCount{
+    _likesCount++;
+}
+
+@end
