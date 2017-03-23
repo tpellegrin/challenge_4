855ba0e95066db5f059b1a065cd0de9142c15cf3
Thiago Pellegrin <thiago@Thiagos-MacBook-Pro.local>
2017-03-23 15:44:08 -0300

    interface/class Likes

---
+++ challenge_4/Likes.m
@@ -0,0 +1,13 @@
+//
+//  Likes.m
+//  challenge_4
+//
+//  Created by Thiago Gomes Pellegrin on 3/23/17.
+//  Copyright © 2017 TP. All rights reserved.
+//
+
+#import "Likes.h"
+
+@implementation Likes
+
+@end
