e75ecb88fea8557ca0d402957d388d15af7f07eb
Thiago Pellegrin <thiago@Thiagos-MacBook-Pro.local>
2017-03-23 15:12:06 -0300

    Initial Commit

---
+++ challenge_4/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  challenge_4
+//
+//  Created by Thiago Gomes Pellegrin on 3/23/17.
+//  Copyright © 2017 TP. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
