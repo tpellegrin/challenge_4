bcefe8b88dc2301cdb63d796206379cbeb1515c7
Virgilius Santos <virgilius-santos@users.noreply.github.com>
2017-03-23 21:05:27 -0300

    criados metodos get e add

--- challenge_4/User.m
+++ challenge_4/User.m
@@ -6,8 +6,47 @@
 //  Copyright © 2017 TP. All rights reserved.
 //
 
+
+
+
 #import "User.h"
+
+
+@interface User ()
+
+@property NSMutableArray *tweets;
+@property NSMutableArray *likes;
+
+@end
 
 @implementation User
 
+- (instancetype)initWithName:(NSString *)nickname
+{
+    self = [super init];
+    if (self) {
+        _nickname = nickname;
+        _tweets = [NSMutableArray new];
+        _likes = [NSMutableArray new];
+    }
+    return self;
+}
+
+- (void) addTweet:(Tweet *)tweet{
+  [tweets addObject:tweet]; 
+}
+
+- (void) addLike:(Likes *)like{
+  [likes addObject:like]; 
+}
+
+- (NSArray *) getLikesList{
+  return [likes copy];
+}
+
+- (NSArray *) getTweetsList{
+  return [tweets copy];
+}
+
+  
 @end
