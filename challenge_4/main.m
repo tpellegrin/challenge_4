8bd1cd7e1d18ea35aeaf4b0c56c4b514eae454d1
Virgilius Santos <virgiliussantos@MacBook-Pro-de-Virgilius.local>
2017-03-24 13:06:20 -0300

    teste de entrada

--- challenge_4/main.m
+++ challenge_4/main.m
@@ -18,9 +18,16 @@ int main(int argc, const char * argv[]) {
         int process = 0;
         
         do {
+            char word [40];
+            scanf("%s",word);
             
+            NSString * userInput = [[NSString alloc] initWithCString: word encoding: NSUTF8StringEncoding];
             
-            
+            long long scannedNumber;
+            NSScanner *scanner = [NSScanner scannerWithString:userInput];
+            [scanner scanLongLong:&scannedNumber];
+            NSNumber *number = [NSNumber numberWithLongLong: scannedNumber];
+            NSLog(@"%@", number);
         } while (process!=0);
         
     }
